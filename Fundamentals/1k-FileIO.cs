6c28a879121f15ffa48b5d805c7d3e30471de847
wangden693 <tamangmilan328@gmail.com>
2022-12-14 07:38:56 +0545

    class work

---
+++ Fundamentals/1k-FileIO.cs
@@ -0,0 +1,32 @@
+using System.IO;
+
+class FileIO
+{
+    string folderPath = @"D:\Apps\.NET Training 5";
+    public void CreateFile()
+    {
+        string fullFilePath = @$"{folderPath}\abc.txt";
+        File.WriteAllText(fullFilePath, "This is first text file");
+    }
+
+    public void CreateDirectory()
+    {
+        string newFolderPath = folderPath + "\\abc";
+        Directory.CreateDirectory(newFolderPath);
+    }
+
+    public void CreateDirectoriesAndFiles()
+    {
+        // CW - Create ten folders named Folder 1, Folder 2 and so on, inside some root folder
+        {
+            string rootFolderPath = folderPath + "\\Folder";
+
+            for (int i = 1; i < 11; i++)
+            {
+                string childFolderPath = "\\Folder" + i;
+                Directory.CreateDirectory(childFolderPath);
+            }
+
+        }
+    }
+}
